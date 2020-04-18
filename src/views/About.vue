17eb137c1254a57dea9d2d178cf6fcf7f67bc519
Geon George <geongeorge98@gmail.com>
2020-04-18 21:53:01 +0530

    added pattern

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="about">
     <p>Uses Giphy</p>
-    <p>Source code here</p>
+    <p>
+      <a href="https://github.com/geongeorge/Words2Gif" target="_blank"
+        >Source code here</a
+      >
+    </p>
     <p class="mt-10">
       <router-link class="font-bold underline" to="/">go back home</router-link>
     </p>
